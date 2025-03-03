e96c910bb52d8b4da9e49dc4faf81f4afdfa74dd
“Edoardo <edoardo.gambacorta@gmail.com>
2025-03-03 12:51:13 +0100

    -

---
+++ imghdr.py
@@ -0,0 +1,41 @@
+def what(file, h=None):
+    return None
+
+def test_jpeg(h, f):
+    return None
+
+def test_png(h, f):
+    return None
+
+def test_gif(h, f):
+    return None
+
+def test_tiff(h, f):
+    return None
+
+def test_rgb(h, f):
+    return None
+
+def test_pbm(h, f):
+    return None
+
+def test_pgm(h, f):
+    return None
+
+def test_ppm(h, f):
+    return None
+
+def test_rast(h, f):
+    return None
+
+def test_xbm(h, f):
+    return None
+
+def test_bmp(h, f):
+    return None
+
+def test_webp(h, f):
+    return None
+
+def test_exr(h, f):
+    return None
